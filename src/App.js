53fd7bcf971123d4f3495131b693806c5607587d
dhsdev <lua@gnomo.net>
2023-11-05 20:54:55 +0100

    Enviroment Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap"
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className="text-center tahoma my-2"> Ricky & Morty</h1>
+      <p className="text-center tahoma"> by <span className="text-primary">Lua Trevín</span></p>
+      
+      
+      React</div>
+  );
+}
+
+export default App;

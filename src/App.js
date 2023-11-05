49f836021683f1a1973389535b3a89e7c9819e21
dhsdev <lua@gnomo.net>
2023-11-05 23:47:16 +0100

    Fixed pagination and implemented search

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import "bootstrap/dist/js/bootstrap"
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 import Pagination from './components/Pagination/Pagination';
-
+import Search from './components/Search/Search';
 
 
 function App() {
 // Data Variables
   let [pageNum, updatePageNum] = useState(1);  
   let [fetchedData, updateFetchedData] = useState([]);
+  let [search, setSearch] = useState("");
   // Break to send to the components info
   //    info -> pagination
   //    results -> cards
   let { info, results } = fetchedData; 
   
 // Template URL to insert variables
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNum}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNum}&name=${search}`;
   
   useEffect(() => {
     (async function () {
@@ -35,7 +36,8 @@ function App() {
       
       <h1 className="text-center tahoma my-2"> Ricky & Morty</h1>
       <p className="text-center tahoma"> by <span className="text-primary">Lua Trevín</span></p>
-      
+      <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} updatePageNum={updatePageNum} />
       <div className="container">
         <div className="row">
           <div className="col-3">
@@ -50,7 +52,6 @@ function App() {
         </div>
         <Pagination updatePageNum={updatePageNum}/>
       </div>
-   
   );
 }
 
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({updatePageNum}) => {
+const Pagination = ({ pageNum, updatePageNum }) => {
   let next = () => {
     updatePageNum((page) => page + 1);
   };
   let prev = () => {
     updatePageNum((page) => page - 1);
   };
-  return ( 
+  return (
     <div className="container d-flex justify-content-center gap-5 my-5">
-      <button onClick={prev} className="btn btn-primary">◀</button>
-      <button onClicl={next} className="btn btn-primary">▶</button>
+      <button onClick={prev} className="btn btn-primary">
+        ◀
+      </button>
+      <button onClick={next} className="btn btn-primary">
+        ▶
+      </button>
     </div>
   );
 };
